85023bbc3573328ccb21c146fc2b447018365e3e
anonymity000 <151495231+anonymity000@users.noreply.github.com>
2023-11-27 02:49:38 +0800

    Add files via upload

---
+++ datasets/__init__.py
@@ -0,0 +1,11 @@
+from .datasets import make
+from . import mini_imagenet
+from . import tiered_imagenet
+from . import cifar
+from . import cub
+from . import vggflower
+from . import aircraft
+from . import fungi
+from . import image_folder
+from . import samplers
+
---
+++ datasets/aircraft.py
@@ -0,0 +1,84 @@
+import os
+import pickle
+from PIL import Image
+
+import torch
+import numpy as np
+from torch.utils.data import Dataset
+from torchvision import transforms
+
+from .datasets import register
+
+
+def index_classes(items):
+    idx = {}
+    for i in items:
+        if (i not in idx):
+            idx[i] = len(idx)
+    return idx
+
+@register('aircraft')
+class Aircraft(Dataset):
+
+    def __init__(self, root_path, split='train', **kwargs):
+        split_tag = split
+        if split == 'train':
+            split_tag = 'train_phase_train'
+        split_file = 'aircraft_category_split_{}.pickle'.format(split_tag)
+        print(split_file)
+        with open(os.path.join(root_path, split_file), 'rb') as f:
+            pack = pickle.load(f, encoding='latin1')
+        data = pack['data']
+        label = pack['label']
+
+        image_size = 80
+        data = [Image.fromarray(np.uint8(x)) for x in data]
+              
+        y = label
+        self.data = data
+        self.label = y
+        self.n_classes= max(self.label)-min(self.label)+1
+
+        norm_params = {'mean': [0.485, 0.456, 0.406],
+                       'std': [0.229, 0.224, 0.225]}
+        normalize = transforms.Normalize(**norm_params)
+        self.default_transform = transforms.Compose([
+            transforms.Resize((image_size,image_size)),
+            transforms.ToTensor(),
+            normalize,  
+        ])
+        
+        augment = kwargs.get('augment')
+       
+        if augment == 'resize':
+            self.transform = transforms.Compose([
+                transforms.RandomResizedCrop(image_size),
+                transforms.RandomHorizontalFlip(),
+                transforms.ToTensor(),
+                normalize,
+            ])
+        elif augment == 'crop':
+            self.transform = transforms.Compose([
+                transforms.Resize(image_size),
+                transforms.RandomCrop(image_size, padding=8),
+                transforms.RandomHorizontalFlip(),
+                transforms.ToTensor(),
+                normalize,
+            ])
+        elif augment is None:
+            self.transform = self.default_transform
+
+        def convert_raw(x):
+            mean = torch.tensor(norm_params['mean']).view(3, 1, 1).type_as(x)
+            std = torch.tensor(norm_params['std']).view(3, 1, 1).type_as(x)
+            return x * std + mean
+        self.convert_raw = convert_raw
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, i):
+        return self.transform(self.data[i]), self.label[i]
+        
+    
+
